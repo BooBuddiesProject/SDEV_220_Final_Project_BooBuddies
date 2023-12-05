8d42afe255a45e7fc353aaa65edd5ac86b109125
mkirk50 <117764969+mkirk50@users.noreply.github.com>
2023-12-04 19:57:13 -0500

    Add files via upload

--- BooBuddies/BooBuddies/views.py
+++ BooBuddies/BooBuddies/views.py
@@ -3,18 +3,33 @@ Routes and views for the flask application.
 """
 
 from datetime import datetime
-from flask import render_template
+from flask import render_template, request, url_for
 from BooBuddies import app
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods =["GET",'PUT', "POST"])
+@app.route('/home', methods =["GET",'PUT', "POST"])
 def home():
+    if request.method == "POST":         
+       search_string = request.form.get("search-keyword")
+       print(search_string)
+       return "You searched for: " + str(search_string) 
+    
     """Renders the home page."""
     return render_template(
         'index.html',
         title='Home Page',
         year=datetime.now().year,
     )
+
+
+# @app.route('/', methods =["GET", "POST"])
+# def gfg():
+
+#        # getting input with name = fname in HTML form
+#        search_string = request.form.get("search-field")
+       
+#        return "Your name is "+first_name + last_name
+#     return render_template("form.html")
 
 @app.route('/contact')
 def contact():
